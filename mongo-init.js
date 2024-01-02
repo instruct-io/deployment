fe03e2abb8cb86cedd8a6f3c51ce6347eb0b398e
BenjaminHerrera <BenHerrera1044@outlook.com>
2024-01-02 15:57:16 -0700

    Update submodules to the latest commits on their main branches

---
+++ mongo-init.js
@@ -0,0 +1,10 @@
+db.createUser({
+    user: 'NaviaCaspar',
+    pwd: 'KleeAndRazorB52Team',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'Instruct'
+        }
+    ]
+});
